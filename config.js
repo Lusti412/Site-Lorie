f3efd9a05159ea39dc5ac3574bdfc905d82a3fe8
Lusti412 <sqounizz@gmail.com>
2026-06-12 01:54:23 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,33 @@
+const CONFIG = {
+  googleScriptURL: "TON_URL_EXEC",
+
+  page1: {
+    image: "./hasarddedingue.png",
+    text: "Texte page 1",
+    yes: "Oui",
+    no: "Non"
+  },
+
+  pageNon: {
+    image: "quielleestcellela.jpeg",
+    text: "Tu as choisi Non",
+    choice1: "Choix 1",
+    choice2: "Choix 2"
+  },
+
+  pageNonChoice1: { text: "Texte choix 1" },
+  pageNonChoice2: { text: "Texte choix 2" },
+
+  pageOui: {
+    image: "chatrose.gif",
+    text: "Tu as choisi Oui",
+    placeholder: "Écris ici..."
+  },
+
+  step2: {
+    text: "Dernier choix",
+    option1: "Option A",
+    option2: "Option B",
+    final: "Message final ❤️"
+  }
+};
