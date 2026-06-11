85a7adbfd01ff63c8ed7a59d78eb62c10b807fb6
Lusti412 <sqounizz@gmail.com>
2026-06-12 01:23:17 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  googleScriptURL: "TON_URL_GOOGLE_SCRIPT",
+  googleScriptURL: "https://script.google.com/macros/s/AKfycbzWVUGK9MIgXO6NoFlrhh5o51nEbA5FCBWlEmCwgjPWZdzS8V4w217F29_FmtaKVa5f/exec",
 
   page1: {
     image: "./hasarddedingue.png",
