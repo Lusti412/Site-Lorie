c878eb2af758373b3bff6c2fccf4b449b2423aa0
Lusti412 <sqounizz@gmail.com>
2026-06-12 01:14:48 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,38 +1,35 @@
 const CONFIG = {
-  googleScriptURL: "https://script.google.com/macros/s/AKfycbwdaJHg6_h6Bw3JcuOj7QSqkcj0w3WJ1Yj_GkISQPryipzs2U2so2LZuMAWVTTUC2Q1/exec",
+  googleScriptURL: "TON_URL_GOOGLE_SCRIPT",
 
   page1: {
-    image: "./hasarddedingue.png",
+    image: "./chat-rose.png",
     text: "TON TEXTE PAGE 1",
     yesText: "Oui",
     noText: "Non"
   },
 
   pageNon: {
-    image: "quielleestcellela.jpeg",
-    text: "TON TEXTE PAGE NON",
+    image: "./hasarddedingue.png",
+    text: "TON TEXTE NON",
     choice1: "Choix 1",
     choice2: "Choix 2"
   },
 
-  pageNonChoice1: {
-    text: "Texte choix 1"
-  },
-
-  pageNonChoice2: {
-    text: "Texte choix 2"
-  },
+  pageNonChoice1: { text: "Texte choix 1" },
+  pageNonChoice2: { text: "Texte choix 2" },
 
   pageOui: {
-    image: "chatrose.gif",
-    text: "TON TEXTE OUI",
-    inputPlaceholder: "Écris ici..."
+    image: "./qui-elle-est-celle-la.jpeg",
+    text: "TON TEXTE OUI"
   },
 
   pageOuiStep2: {
-    text: "TON TEXTE AVEC CHOIX",
+    text: "TON TEXTE ETAPE 2",
     option1: "Option A",
-    option2: "Option B",
-    finalText: "Merci et à lundi ❤️"
+    option2: "Option B"
+  },
+
+  finalPage: {
+    text: "TON MESSAGE FINAL PERSONNALISÉ ❤️"
   }
 };
