0487d114b56cabdbbc5596fc8927960196088db5
Lusti412 <sqounizz@gmail.com>
2026-06-12 00:18:24 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,31 +2,29 @@ const CONFIG = {
   googleScriptURL: "https://script.google.com/macros/s/AKfycbwdaJHg6_h6Bw3JcuOj7QSqkcj0w3WJ1Yj_GkISQPryipzs2U2so2LZuMAWVTTUC2Q1/exec",
 
   page1: {
-    image: "chat rose.gif",
+    image: "hasarddedingue.gif",
     text: "TON TEXTE PAGE 1 ICI",
     yesText: "Oui",
     noText: "Non"
   },
 
   pageNon: {
-    image: "hasard de dingue.gif",
+    image: "quielleestcellela.jpeg",
     text: "TON TEXTE PAGE NON ICI",
     choice1: "Choix 1",
     choice2: "Choix 2"
   },
 
   pageNonChoice1: {
-    image: "hasard de dingue.gif",
     text: "Texte choix 1"
   },
 
   pageNonChoice2: {
-    image: "hasard de dingue.gif",
     text: "Texte choix 2"
   },
 
   pageOui: {
-    image: "qui elle est celle la.jpeg",
+    image: "chatrose.gif",
     text: "TON TEXTE PAGE OUI ICI",
     placeholder: "Écris ici...",
     option1: "Option A",
